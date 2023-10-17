1db2a9bf1889816d92cbebb254aa0a6cd772631d
Jean-Francois Rieu <jfrieu@fr.ibm.com>
2023-10-17 13:17:08 +0200

    Put the Jar file

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "d213786f-55a7-4ed0-944f-480b4dcf3caa";
+private static final String ID = "35b048f2-06e2-45a2-8e78-008de7e2ed04";
 }
